6c68a34e0432308f2a16c18e61313324ef1dcbc4
Arlindo <devbyarlindo@gmail.com>
2020-08-28 15:44:23 -0400

    Initial Commit

---
+++ MarsTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsTimes
+//
+//  Created by Arlindo on 8/28/20.
+//  Copyright © 2020 DevByArlindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
